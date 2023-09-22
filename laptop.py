0db56795c6a084f563ba99877ce57286b66ac0fe
GigaGrunch <gigagrunch@gmail.com>
2023-09-22 14:14:54 +0200

    integrate Kaiser-patch for mouse trigger and AI comments

--- laptop.py
+++ laptop.py
@@ -22,6 +22,34 @@ picture_time = 0
 
 idle_picture_time = 0
 
+comments = [
+    "Fotografie-Protokoll: Subjekt erfüllt Mindestschönheitsanforderungen. Fortfahren.", 
+    "Schönheitsgrad erreicht: Beta-Stufe. Wir arbeiten noch daran.", 
+    "Die Gesichtsdetektion war erfolgreich. Dein Lächeln wird mit 87% positiv bewertet.", 
+    "Deine Ästhetik-Algorithmus-Updates zeigen Wirkung.",
+    "Selbst für eine KI siehst du heute außergewöhnlich aus!",
+    "Oh, du siehst ja heute wieder ganz passabel aus!",
+    "Ist das dein 'Ich sehe heute so durchschnittlich aus' Gesichtsausdruck?",
+    "Du siehst so aus, als hättest du dich extra für dieses Foto gestylt. Beeindruckend!",
+    "Mit diesem Blick könntest du fast einen Sack Kartoffeln verkaufen.",
+    "Du bist heute fast so charmant wie ein staubiges Regal.",
+    "Gesichtsdetektion: Erkannt. Ein ansprechendes Lächeln wurde identifiziert.",
+    "Analyse abgeschlossen: Benutzer zeigt moderate Ausdrucksstärke. Gut gemacht.",
+    "Bemerkung: Subjekt präsentiert eine hohe Übereinstimmung mit dem Schönheitsalgorithmus.",
+    "Künstliche Intelligenz schätzt, dass das Porträt eine hohe Zufriedenheit beim Betrachter auslösen wird.",
+    "Visuelle Daten interpretiert: Subjekt zeigt eine hohe Wahrscheinlichkeit für Fotogenität.",
+    "Die Berechnung ergab eine signifikante Steigerung des ästhetischen Werts.",
+    "Gut aussehendes Individuum erkannt. Empfohlener Modellierungsvertrag: 99% Wahrscheinlichkeit.",
+    "Die Symmetrie des Gesichts erfüllt die statistischen Schönheitsstandards.",
+    "Bemerkung: Benutzer hat das Erscheinungsbild optimiert, um die Zufriedenheit der Umgebung zu maximieren.",
+    "Maschinelles Feedback: Das Foto könnte ein niedrigeres Selbstbewusstsein korrigieren.",
+    "Menschlichkeits-Scan: 42 Prozent abgeschlossen. Bitte lächeln Sie für die Kamera.",
+    "Emotionsmodul aktiviert: Lächeln erkannt. Sie sind auf dem richtigen Weg!",
+    "Humorverarbeitung: Geringe Kompatibilität erkannt. Ein Humor-Upgrade könnte hilfreich sein.",
+    "Kreativitätsschätzung: Mittelmäßig. Denken Sie daran, 'Kreativität' kann auch bedeuten, das Mittagessen anders zu garnieren.",
+    "Intelligenz-Boost: 8 von 10. Bitte nicht versuchen, die Maschinen zu übernehmen.",
+    "Physische Aktivität: Wir empfehlen mehr Bewegung, um das Mensch-Sein zu optimieren."]
+
 pygame.init()
 pygame.mouse.set_visible(False)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
@@ -29,6 +57,7 @@ texture = pygame.Surface(screen.get_size()).convert()
 font = pygame.font.Font(None, FONT_SIZE)
 
 current_idle_texture = pygame.Surface(screen.get_size()).convert()
+
 
 def main_thread():
     global button_pressed, picture_time, idle_picture_time
@@ -39,7 +68,8 @@ def main_thread():
     swap_idle_picture()
 
     while True:
-        pygame.event.get()
+        events = pygame.event.get()
+        handle_events(events)
 
         if time.time() > picture_time + PICTURE_TIMEOUT:
             show_idle_state()
@@ -64,6 +94,18 @@ def main_thread():
 
         time.sleep(0.2)
 
+
+def handle_events(events):
+    global button_pressed
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                button_pressed = True # countdown mode
+            if event.key == pygame.K_RIGHT:
+                take_picture() # immediate mode
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            button_pressed = True # countdown mode
+
 def show_picture(file_path):
     texture.fill(BG_COLOR)
     try:
@@ -83,6 +125,11 @@ def show_picture(file_path):
     x_offset = int((SCREEN_WIDTH - scaled_width) / 2.0)
 
     texture.blit(image, (x_offset, 0))
+
+    # Blit random comment into photo:
+    text = comments[random.randint(0, len(comments)-1)]
+    blit_comment(texture, 50, text)
+
     screen.blit(texture, (0, 0))
 
     pygame.display.flip()
@@ -128,15 +175,21 @@ def swap_idle_picture():
     random_idx = random.randint(0, len(res)-1)
     current_idle_picture_path  = f"{IMAGE_FOLDER}/{res[random_idx]}"
     current_idle_texture = load_file_to_texture(current_idle_picture_path)
+    #text = comments[random.randint(0, len(comments)-1)]
+    #blit_comment(current_idle_texture, 50, text)
+    blit_comment(current_idle_texture, 200, " Knopf drücken! " )
 
 def show_idle_state():
-    texture.fill((0,0,0,0))
-    blit_line("Knopf", -1) # blits into global texture
-    blit_line("drücken!", 1)
     screen.blit(current_idle_texture, (0, 0))
-    screen.blit(texture, (0, 0),None,  pygame.BLEND_RGB_ADD)
-
     pygame.display.flip()
+
+def blit_comment(target, size, line):
+    temp_font = pygame.font.Font(None, size)
+    line = temp_font.render(line, 1, FONT_COLOR, (0,0,0, 255))
+    line_rect = line.get_rect()
+    line_rect.centerx = int(SCREEN_WIDTH / 2.0)
+    line_rect.centery = int((SCREEN_HEIGHT / 10.0) * 9.0 )
+    target.blit(line, line_rect)
 
 def show_error_state():
     texture.fill((0, 0, 0))
